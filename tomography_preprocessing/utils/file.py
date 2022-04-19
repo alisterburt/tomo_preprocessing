44f149b300d271c83d59ce69536354bc31e60ebf
Alister Burt <alisterburt@gmail.com>
2022-04-19 17:50:14 +0100

    add return type to basename

--- tomography_preprocessing/utils/file.py
+++ tomography_preprocessing/utils/file.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import List
 
 
-def basename(path: Path):
+def basename(path: Path) -> str:
     while path.stem != str(path):
         path = Path(path.stem)
     return path
