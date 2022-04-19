cbe168996cc4a5f7f989f615b86c41b25df07f88
Alister Burt <alisterburt@gmail.com>
2022-04-19 17:45:30 +0100

    add IMOD transformation extraction utils

--- tomography_preprocessing/utils/imod.py
+++ tomography_preprocessing/utils/imod.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def read_xf(file: os.PathLike) -> np.ndarray:
-    """Read an IMOD xf file into a numpy array.
+    """Read an IMOD xf file into an (n, 6) numpy array.
 
     The file with alignment transforms (option OutputTransformFile) contains one
     line per view, each with a linear transformation specified by six numbers:
@@ -13,14 +13,48 @@ def read_xf(file: os.PathLike) -> np.ndarray:
         X' = A11 * X + A12 * Y + DX
         Y' = A21 * X + A22 * Y + DY
     """
-    return np.loadtxt(fname=file, dtype=float)
+    return np.loadtxt(fname=file, dtype=float).reshape((-1, 6))
 
 
-def xf_to_shifts(xf: np.ndarray):
-    """Extract XY shifts from IMOD xf data."""
-    return xf[:, (-2, -1)]
+def read_tlt(file: os.PathLike) -> np.ndarray:
+    """Read an IMOD tlt file into an (n, ) numpy array."""
+    return np.loadtxt(fname=file, dtype=float).reshape(-1)
 
 
-def xf_to_transformation_matrix(xf: np.ndarray) -> np.ndarray:
-    """Extract the transformation matrix from IMOD xf data."""
-    return xf[:, 0:4]
+def get_xf_shifts(xf: np.ndarray) -> np.ndarray:
+    """Extract XY shifts from IMOD xf data.
+
+    Output is an (n, 2) numpy array of shifts which center tilt-images.
+    """
+    transformation_matrices = get_xf_transformation_matrices(xf)
+    post_transformation_shifts = xf[:, -2:].reshape((-1, 2, 1))
+    pre_transformation_shifts = transformation_matrices @ post_transformation_shifts
+    return pre_transformation_shifts.reshape((-1, 2))
+
+
+def get_xf_transformation_matrices(xf: np.ndarray) -> np.ndarray:
+    """Extract the 2D transformation matrix from IMOD xf data.
+
+    Output is an (n, 2, 2) numpy array of matrices.
+    """
+    return xf[:, :4].reshape((-1, 2, 2))
+
+
+def get_xf_rotation_angles(xf: np.ndarray) -> np.ndarray:
+    """Extract the in plane rotation angle from IMOD xf data.
+
+    Output is an (n, ) numpy array of angles in degrees. This function assumes
+    that the transformation in the xf file is a simple 2D rotation.
+    """
+    transformation_matrices = get_xf_transformation_matrices(xf)
+    cos_theta = transformation_matrices[:, 0, 0]
+    return np.rad2deg(np.arccos(cos_theta))
+
+
+
+
+
+
+
+
+
