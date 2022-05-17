75211f687c3a076ff1d4dbacdd7155ebfc0eac23
Alister Burt <alisterburt@gmail.com>
2022-05-18 00:51:18 +0100

    flake aretomo.py

--- tomography_preprocessing/tilt_series_alignment/aretomo.py
+++ tomography_preprocessing/tilt_series_alignment/aretomo.py
@@ -1,29 +1,28 @@
 from pathlib import Path
 from typing import Optional
 
-import typer
 import rich
-import sys
-
+import typer
 from lil_aretomo.aretomo import run_aretomo_alignment
+
 from ._cli import cli
 from .. import utils
 from ..utils.relion import relion_pipeline_job
-from rich.progress import track
 
 console = rich.console.Console(record=True)
+
 
 @cli.command(name='AreTomo')
 @relion_pipeline_job
 def aretomo_function(
         tilt_series_star_file: Path = typer.Option(...),
         output_directory: Path = typer.Option(...),
-        aretomo_executable: Path = typer.Option(...),	
+        aretomo_executable: Path = typer.Option(...),
         local_align: Optional[bool] = typer.Option(False),
         target_pixel_size: Optional[float] = typer.Option(10),
-        n_patches_xy: Optional[tuple[int,int]] = typer.Option((5,4)),
+        n_patches_xy: Optional[tuple[int, int]] = typer.Option((5, 4)),
         correct_tilt_angle_offset: Optional[bool] = typer.Option(False),
-        thickness_for_alignment: Optional[float] = typer.Option(800),	
+        thickness_for_alignment: Optional[float] = typer.Option(800),
         tomogram_name: Optional[str] = typer.Option(None)
 ):
     console.log('Extracting metadata for all tilt series.')
@@ -33,17 +32,17 @@ def aretomo_function(
     )
     for tilt_series_id, tilt_series_df, tilt_image_df in tilt_series_metadata:
         console.log(f'Aligning {tilt_series_id}...')
-        	
+
         utils.aretomo.align_single_tilt_series(
             tilt_series_id=tilt_series_id,
             tilt_series_df=tilt_series_df,
             tilt_image_df=tilt_image_df,
             alignment_function=run_aretomo_alignment,
             aretomo_executable=aretomo_executable,
-	    local_align=local_align,
-	    target_pixel_size=target_pixel_size,
-	    n_patches_xy=n_patches_xy,
-	    correct_tilt_angle_offset=correct_tilt_angle_offset,
+            local_align=local_align,
+            target_pixel_size=target_pixel_size,
+            n_patches_xy=n_patches_xy,
+            correct_tilt_angle_offset=correct_tilt_angle_offset,
             thickness_for_alignment=thickness_for_alignment,
             output_directory=output_directory,
         )
