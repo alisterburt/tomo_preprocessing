6f2030b03f34ad50ce8289951a5c0b32bc8a9588
alisterburt <alisterburt@gmail.com>
2022-05-19 14:47:55 +0100

    expose generate matrices

--- tomography_preprocessing/tilt_series_alignment/__init__.py
+++ tomography_preprocessing/tilt_series_alignment/__init__.py
@@ -2,6 +2,7 @@
 from .aretomo import batch_aretomo as _batch_aretomo
 from .imod import batch_fiducials as _batch_imod_fiducials
 from .imod import batch_patch_tracking as _batch_imod_patch_tracking
+from .imod import generate_matrices as _generate_imod_matrices
 
 # Then import the cli, it will be decorated with all programs
 from ._cli import cli
