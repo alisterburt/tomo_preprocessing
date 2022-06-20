c99619c0fde3dbdd01e15f9a515e2e6681695a65
EuanPyle <euanpyle@gmail.com>
2022-06-20 14:51:30 +0100

    If user enters a space in gpu_id input string, now converted to nothing to prevent errors

--- tomography_preprocessing/tilt_series_alignment/aretomo/_utils.py
+++ tomography_preprocessing/tilt_series_alignment/aretomo/_utils.py
@@ -4,4 +4,5 @@ def gpu_ids_string2tuple(
         gpu_ids: str
     ) -> Tuple:
     """Convert GPU  ID input from colon spaced (1:2:3) to no space inbetween GPU IDs (123)"""
+    gpu_ids = gpu_ids.replace(' ','')
     return tuple(map(int,gpu_ids.replace(':','')))
