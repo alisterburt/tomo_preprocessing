a7beec171d365361690d2f828b3b8f35f22f86e9
EuanPyle <euanpyle@gmail.com>
2022-07-07 13:35:33 +0100

    First working generation of config json

--- tomography_preprocessing/denoising/__init__.py
+++ tomography_preprocessing/denoising/__init__.py
@@ -1,3 +1,2 @@
-from .split_tilt_series import split_tilt_series as _split_tilt_series
 from .care4relion import care4relion as _care4relion
 from ._cli import cli
--- tomography_preprocessing/denoising/care4relion/care4relion.py
+++ tomography_preprocessing/denoising/care4relion/care4relion.py
@@ -4,6 +4,7 @@ import pandas as pd
 import starfile
 import rich
 import typer
+import json
 from rich.progress import track
 
 from .._cli import cli
@@ -16,6 +17,7 @@ console = rich.console.Console(record=True)
 def care4relion(
     tilt_series_star_file: Path = typer.Option(...),
     output_directory: Path = typer.Option(...),
+    training_tomograms: str = typer.Option(...),
 ):
     """CARE
 
@@ -23,6 +25,8 @@ def care4relion(
     ----------
     tilt_series_star_file: RELION tilt-series STAR file.
     output_directory: directory in which results will be stored.
+    training_tomograms: tomograms which are to be used for denoising neural network training.
+        User should enter tomogram names as rlnTomoName, separated by colons, e.g. TS_1:TS_2
 
     Returns
     -------
@@ -33,13 +37,44 @@ def care4relion(
         console.log(f'ERROR: {e}')
         raise RuntimeError(e)    
       
-    #Detect _rlnTomoDenoisingSplitTomogram Exists, if not, recommend split tomo must be fed to Rec Tomo and use the output of that here
+    global_star = starfile.read(tilt_series_star_file, always_dict=True)['global']
     
-    #Use _rlnTomoDenoisingSplitTomogram to find both halves of tomogram
+    if not 'rlnTomoTomogramHalvesForDenoising' in global_star.columns:
+        e = 'Could not find rlnTomoTomogramHalvesForDenoising in tilt series star file.'
+        console.log(f'ERROR: {e}')
+        raise RuntimeError(e)
     
-    #Generate json file for train_data_config.json
+    #=====Function 1
+    training_tomograms = training_tomograms.strip().split(':')
+    #=====/Function 1
+    
+    #=====Function 2
+    training_tomograms_idx = pd.DataFrame(global_star.rlnTomoName.tolist()).isin(training_tomograms).values
+    if not any(training_tomograms_idx):
+        e = f"Could not user specified training tomograms ({', '.join(str(x) for x in training_tomograms)}) in tilt series star file"
+        console.log(f'ERROR: {e}')
+        raise RuntimeError(e)
+    training_tomograms_star = global_star[training_tomograms_idx]
+    #=====/Function 2
+    
+    #=====Function 3
+    even_tomos = [f'{rows.rlnTomoTomogramHalvesForDenoising}_even.mrc' for idx,rows in training_tomograms_star.iterrows()]
+    odd_tomos = [f'{rows.rlnTomoTomogramHalvesForDenoising}_odd.mrc' for idx,rows in training_tomograms_star.iterrows()]
+    #=====/Function 3
+    
+    #Training_dir
+    training_dir = Path('test') ############
     
     #Add other user defined options, and let user define training tomograms and GPU ID
+
+    #=====Function 4
+    train_data_config_json_template = json.loads(f'{{"even": {json.dumps(even_tomos)}, "odd": {json.dumps(odd_tomos)}, "patch_shape": [72, 72, 72], \
+    "num_slices": 250, "split": 0.9, "tilt_axis": "Y", "n_normalization_samples": 50, "path": "{str(training_dir)}"}}')
+    #=====/Function 4
+    
+    with open(f'{str(training_dir)}/train_data_config.json', 'w') as outfile:
+        json.dump(train_data_config_json_template, outfile, indent=4)   
+    
     
     #Run config.json
     
