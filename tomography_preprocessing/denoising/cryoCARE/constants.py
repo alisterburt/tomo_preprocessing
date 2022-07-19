df92b7a0aad22681670f08a5609d0de36da56500
EuanPyle <euanpyle@gmail.com>
2022-07-19 16:02:19 +0100

    Forgot to change EVEN_SUFFIX to _half1

--- tomography_preprocessing/denoising/cryoCARE/constants.py
+++ tomography_preprocessing/denoising/cryoCARE/constants.py
@@ -1,4 +1,4 @@
-EVEN_SUFFIX = '_even' #The suffix given to even tomograms during split tomogram generation. This will be removed from the names of the output tomograms
+EVEN_SUFFIX = '_half1' #The suffix given to even tomograms during split tomogram generation. This will be removed from the names of the output tomograms
 
 PREDICT_CONFIG_PREFIX = 'predict_config' #Name (minus the suffix) of the predict_config.json file
 
